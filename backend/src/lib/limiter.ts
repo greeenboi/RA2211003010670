1081b302c7203133297224629ad72f2b2c267dc4
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-03-20 19:22:29 +0530

    add Redis rate limiter implementation

---
+++ backend/src/lib/limiter.ts
@@ -0,0 +1,11 @@
+import { Redis } from '@upstash/redis'
+import { RedisStore } from '@hono-rate-limiter/redis'
+import { CONFIG } from "../config"
+
+export const redis = new Redis({
+  url: CONFIG.upstash.UPSTASH_REDIS_REST_URL,
+  token: CONFIG.upstash.UPSTASH_REDIS_REST_TOKEN,
+})
+
+
+export const store = new RedisStore({ client: redis })
