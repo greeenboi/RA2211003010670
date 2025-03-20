f7f4e4f969f4de9ecd79e1698bb8cb80078e8c24
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-03-20 19:22:51 +0530

    add Hono router with health check and user routes

---
+++ backend/src/routes/index.tsx
@@ -0,0 +1,18 @@
+import { Hono } from "hono";
+import usersRouter from "./getUsers";
+
+const router = new Hono();
+
+router.get('/', (c) => {
+    return c.text('Hello Hono!')
+  })
+  
+router.get('/health', (c) => {
+return c.json({ status: 'ok' })
+})
+
+// routes are mapped internally
+
+router.route("/", usersRouter); 
+
+export default router;
