fbb4d9dfe692eca816ccbcdc95270ca97a5c77ad
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-03-20 19:22:23 +0530

    add rate limiting and route display to Hono app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,33 @@
 import { Hono } from 'hono'
+import { cache } from 'hono/cache'
+import { rateLimiter } from 'hono-rate-limiter'
+import { store } from './lib/limiter'
+import { getRouterName, showRoutes } from 'hono/dev';
+import { Bootstrap } from './utils'
+
+
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  '*',
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // i think 15 minutes is enough
+    limit: 100,
+    standardHeaders: 'draft-6',
+    keyGenerator: (c) => c.req.header('x-forwarded-for') ||
+		c.req.header('x-real-ip') ||
+		'default-key',
+    store: store,
+  })
+)
+
+Bootstrap(app)
+
+getRouterName(app);
+showRoutes(app, {
+	verbose: true,
+});
+
 
 export default app
