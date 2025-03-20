c3dbaef67fd80e8b799edf0deacfa614196d7c93
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-03-20 19:22:15 +0530

    add Upstash configuration to backend config file

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,6 +1,8 @@
 import { config } from 'dotenv';
 config();
 
+//env keys are shown since idk if u will have access to it or nah
+
 export const CONFIG = {
     secrets: {
 		companyName: "goMart",
@@ -11,6 +13,10 @@ export const CONFIG = {
         rollNo: "RA2211003010670",
 
 	},
+    upstash: {
+        UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL || 'https://exotic-cowbird-23626.upstash.io',
+        UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN || 'AVxKAAIjcDE0ZDkzZjQ0OTE5NzM0NWI4OWUxMjQwNWUwOGY4MDIyY3AxMA'
+    },
     api: {
         url: "http://20.244.56.144"
     }
